b3323573ae8e618938632f9773aea43728f66eb0
Haram Yoon <haramkim082@gmail.com>
2023-10-05 16:30:30 -0700

    postgres and form setup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,32 +1,90 @@
 import React, { useEffect, useState } from "react";
 
 function index() {
-
+  
   const [message, setMessage] = useState("Loading"); // set state variable
   const [people, setPeople] = useState([]); // make array for 'people' from server.py
 
-  useEffect(() => { // allows us to create side effects in functional components
-    fetch("http://localhost:8080/api/home")
-    .then( // calls the endpoint
-      (response) => response.json() // get response from api, put into json
-    ).then(
-      (data) => { // retrieve data
-        console.log(data); // so we can check console.log and see that we
-                         // are getting the api endpoint data
-        // message initially set to loading in line 5
-        setMessage(data.message); // once we get data, set message state variable to data
-        setPeople(data.people); // accessing people variable from backend
-      });
-  }, []);
+
+  // for post task
+  const [name, setName] = useState("")
+  const [header, setheader] = useState("")
+  const [description, setdescription] = useState("")
+  const [isOpen, setIsOpen] = useState(true)
+  const [event, setEvent] = useState([])
+
+  // useEffect(() => { // allows us to create side effects in functional components
+  //   fetch("http://localhost:8080/api/home")
+  //   .then( // calls the endpoint
+  //     (response) => response.json() // get response from api, put into json
+  //   ).then(
+  //     (data) => { // retrieve data
+  //       console.log(data); // so we can check console.log and see that we
+  //                        // are getting the api endpoint data
+  //       // message initially set to loading in line 5
+  //       setMessage(data.message); // once we get data, set message state variable to data
+  //       setPeople(data.people); // accessing people variable from backend
+  //     });
+  // }, []);
 
   return(
-    <div>
+    <>
+    {/* <div>
       <div>{message}</div>
-      
     {people.map((person, index) => ( // grabs person in people array by index and displays
       <div key ={index}>{person}</div>
       ))}
-    </div>
+
+
+    </div> */}
+
+    <button className="p-3 h-10 bg-blue-300 text-center rounded-lg w-auto " onClick={()=>setIsOpen(!isOpen)}>Add a task</button>
+
+    {isOpen && (
+    <div className="h-96 mt-8 flex items-center justify-center rounded-md bg-slate-500  flex-col gap-3">
+        <div className="text-xl font-bold text-slate-200">Add a task</div>
+
+        <div className="input-container ic1">
+            <input placeholder="Name" type="text" className="w-60 lg:w-80 rounded-sm p-2"
+            value={name} onChange={(e)=> setName(e.target.value)} required/>
+        </div>
+
+        <div className="input-container ic2">
+            <input placeholder="Header" type="text" className="w-60 lg:w-80 rounded-sm p-2" value={header} onChange={(e)=> setheader(e.target.value)} required/>   
+        </div>
+        <div>
+            <textarea placeholder="Add a description.." className="w-60 lg:w-80 rounded-sm p-2"  rows={6}
+            value={description ? description: ""} onChange={(e)=> setdescription(e.target.value)}/>
+        </div>
+
+        <div className="flex">
+        <button
+        type="button"
+        tabIndex={0}
+        className="mt-3 w-24 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:scale-95 ml-3"
+        onClick={() => setIsOpen(false)}
+        >
+        Cancel
+        </button>
+        <button
+        type="submit"
+        tabIndex={0}
+        className="mt-3 w-24 rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-600 hover:scale-95 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ml-3 "
+        onClick={()=>{ 
+          setIsOpen(false); 
+          setEvent([...event, {name: name, header: header, description: description}])
+          setName("")
+          setheader("")
+          setdescription("")
+        }
+        }
+        >
+        Confirm
+        </button>
+        </div>
+    </div>)
+    }
+    </>
   );
 }
 
--- server/server.py
+++ server/server.py
@@ -1,9 +1,51 @@
 # write flask code in python here
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
 
-app = Flask(__name__)
+
+app = Flask(__name__) 
+DATABASE_URL = 'postgresql://temp:temp@localhost:5432/journals'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL  # need to set up database
+db = SQLAlchemy(app)
+db.create_all()
 CORS(app)
+
+# create engine
+engine = create_engine(DATABASE_URL)
+connection = engine.connect()
+
+# Define your "blank" table model
+Base = declarative_base()
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+Base.metadata.create_all(engine)
+
+
+# make journalEntry through sqlAlchemy -- takes id, title, and content
+class JournalEntry(Base):
+    __tablename__ = 'journal_entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50))
+    content = Column(String(500))
+
+# Create a new instance of the Blank model with the data you want to add
+new_entry = JournalEntry(id=12113213, title="journal 1", content="this is my first journal entry")
+
+# Add the new instance to the session
+session.add(new_entry)
+
+# Commit the changes to the database
+session.commit()
+
+for row in session.query.all():
+    print(f"ID: {row.id}, Name: {row.name}, Var: {row.var}")
 
 # make api requests
 @app.route("/api/home", methods=['GET'])
@@ -14,5 +56,17 @@ def return_home():
                    'Angie', 'Abrar', 'Mohamed', 'Grace',
                    'Sadhvi', 'Erin']
     })
+
+# make route for api/journal endpoint
+@app.route("/api/journal", methods=['POST'])
+def add_journal_entry():
+    data = request.get_json()
+    title = data['title']
+    content = data['content']
+    entry = JournalEntry(title=title, content=content)
+    db.session.add(entry)
+    db.session.commit()
+    return 'added to database'
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
