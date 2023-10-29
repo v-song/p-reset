015a6a3a657a74e181ca4549898dbb796129886a
angiezh <angiezhou.az@gmail.com>
2023-10-29 16:24:53 -0700

    venv

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 # write flask code in python here
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +11,7 @@ CORS(app)
 def return_home():
     return jsonify({
         'message': "P-reset starter page!",
-        'people': ['Sumi', 'Vivien', 'Haram', 'Tara'
+        'people': ['Sumi', 'Vivien', 'Haram', 'Tara',
                    'Angie', 'Abrar', 'Mohamed', 'Grace',
                    'Sadhvi', 'Erin']
     })
