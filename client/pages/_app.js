42c0e975ddada590b1179a1e610f3aa9ef09a8db
Mohamed Hamouda <mohamed.hamouda@rebilly.com>
2023-10-28 21:49:56 -0700

    done with authentication, getting acces to users calendar and personal info, and displaying the next 10 events. Next, create events

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,5 @@
 import '@/styles/globals.css'
-import {SessionProvider} from 'next-auth/react'
 
-export default function App({ Component, pageProps, session}) {
-  return (
-  <SessionProvider session={session}>
-    <Component {...pageProps} />
-  </SessionProvider>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,30 +2,56 @@ import React, { useEffect, useState } from "react";
 
 function index() {
 
-  const [message, setMessage] = useState("Loading"); // set state variable
-  const [people, setPeople] = useState([]); // make array for 'people' from server.py
+  const [userInfo, setUserInfo] = useState(null);
+  const [events, setEvents] = useState([]);
 
-  useEffect(() => { // allows us to create side effects in functional components
-    fetch("http://localhost:8080/api/home")
-    .then( // calls the endpoint
-      (response) => response.json() // get response from api, put into json
-    ).then(
-      (data) => { // retrieve data
-        console.log(data); // so we can check console.log and see that we
-                         // are getting the api endpoint data
-        // message initially set to loading in line 5
-        setMessage(data.message); // once we get data, set message state variable to data
-        setPeople(data.people); // accessing people variable from backend
-      });
-  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/user_info', {credentials: 'include'})
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setUserInfo(data);
+        });
+
+    fetch('http://localhost:8080/events', {credentials: 'include'})
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setEvents(data);
+        });
+}, []);
 
   return(
     <div>
-      <div>{message}</div>
-      
-    {people.map((person, index) => ( // grabs person in people array by index and displays
-      <div key ={index}>{person}</div>
-      ))}
+    <button className="button" onClick={() => window.location.href = 'http://localhost:8080/google/login'}>
+      Login with Google
+    </button>
+    
+    <button className="button" onClick={() => window.location.href = 'http://localhost:8080/logout'}>
+      Logout from Google
+    </button>
+    {userInfo && (
+            <div>
+                <h2>User Information</h2>
+                <p><strong>Name:</strong> {userInfo.name}</p>
+                <img src={userInfo.picture} alt="User profile" />
+            </div>
+        )}
+    
+    {events.length > 0 && (
+            <div>
+                <h2>10 Upcoming Events</h2>
+                <ul>
+                    {events.map(event => (
+                        <li key={event.id}>
+                            {event.summary} - {new Date(event.start.dateTime || event.start.date).toLocaleString()}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )}
+    
     </div>
   );
 }
