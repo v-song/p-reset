835763134e7e959bb7ab2c73b2cec9f0ff2e400c
Mohamed Hamouda <mohamed.hamouda@rebilly.com>
2023-11-07 18:32:32 -0800

    Included all the ui and error messages updates that erin did.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import AddEvent from "@/components/add_event";
 
 function index() {
 
   const [userInfo, setUserInfo] = useState(null);
   const [events, setEvents] = useState([]);
+  const calendarEvents = Array.isArray(events) ? 
+  events.map(event => ({
+    title: event.summary,
+    start: new Date(event.start.dateTime),
+    end: new Date(event.end.dateTime),
+  })) : [];
+
 
 
   useEffect(() => {
@@ -17,6 +27,7 @@ function index() {
     fetch('http://localhost:8080/events', {credentials: 'include'})
         .then(response => response.json())
         .then(data => {
+          console.log("now printing")
           console.log(data);
           setEvents(data);
         });
@@ -24,22 +35,35 @@ function index() {
 
   return(
     <div>
-    <button className="button" onClick={() => window.location.href = 'http://localhost:8080/google/login'}>
-      Login with Google
-    </button>
-    
-    <button className="button" onClick={() => window.location.href = 'http://localhost:8080/logout'}>
-      Logout from Google
-    </button>
+      <div className="flex justify-between items-center text-white">
+
+
+      <div className="flex gap-4">
+        <button className="button" onClick={() => window.location.href = 'http://localhost:8080/google/login'}>
+          Login with Google
+        </button>
+        
+        <button className="button" onClick={() => window.location.href = 'http://localhost:8080/logout'}>
+          Logout from Google
+        </button>
+      </div>
+
+
+      <div className="flex gap-4">
+        <button className="button" onClick={() => window.location.href = "http://localhost:3000"}>Link 1</button>
+        <button className="button" onClick={() => window.location.href = "http://localhost:3000"}>Link 2</button>
+        <button className="button" onClick={() => window.location.href = "http://localhost:3000"}>Link 3</button>
+      </div>
+
+    </div>
     <hr></hr>
-    {userInfo && (
+    {/* {userInfo && (
             <div>
                 <img src={userInfo.picture} alt="User profile" />
                 <p>Welcome to P-reset, <strong>{userInfo.given_name}</strong>!</p>
             </div>
-        )}
-    <hr></hr>
-    {events.length > 0 && (
+        )} */}
+    {/* {events.length > 0 && (
             <div>
                 <h2><strong>10 Upcoming Events</strong></h2>
                 <ul>
@@ -50,9 +74,23 @@ function index() {
                     ))}
                 </ul>
             </div>
-        )}
-    
-    <button className="button" onClick={() => window.location.href = "http://localhost:3000/add_event"}>Create Event</button>
+        )} */}
+    <div className="flex h-screen ">
+  {/* Calendar Section */}
+  <div className="w-3/5 -mt-5" >
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={calendarEvents}
+    />
+  </div>
+
+  {/* Right Side Section for Button and Form */}
+  <div className=" p-4">
+    <AddEvent />
+  </div>
+</div>
+
     </div>
   );
 }
